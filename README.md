bbf4be0a1576c8b21f56c07a2eebf04d9d691025
arslanarshad31 <arslanarshad.31@gmail.com>
2017-09-03 01:50:51 +0800

    Improving Docs, Validating Args and Minor Bug Fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # git-fork
 
-This utility can be used to take out a foler from a repository and make it into a new repository.
+This utility can be used to take out a foler from a git repository and make it into a new git repository and eventually provide commands to sync changes between both the repositories.
 
 # Commands
 ### init
@@ -8,7 +8,7 @@ This utility can be used to take out a foler from a repository and make it into 
 $ git-fork init [relative path to repo] [path to folder from root of repo] [name of new folder]
 $ git-fork init ./../test-repo src/frontend/public frontend-public
 ```
-This command creates a new folder in the current directory and initiates a new git repository in it. It copies all the content in the foler `test-repo/src/frontend/public` and places it in it `frontend-public/src/frontend/public`. This command also creates a config file `frontend-public/git-fork.json` to store configuration details.
+This command creates a new folder in the current directory and initiates a new git repository in it. It copies all the content from `test-repo/src/frontend/public` and places it in `frontend-public/src/frontend/public`. In addition, this command also creates a config file `frontend-public/git-fork.json` to store configuration details.
 
 
 ### pull
@@ -21,7 +21,7 @@ If `test-repo/src/frontend/public` is updated, this command can be used to sync 
 ### push
 ```sh
 $ cd frontend-public
-$ git-fork push [commit message]
-$ git-fork push updated-photos
+$ git-fork push [commit message] [branch message]
+$ git-fork push updated-photos photos
 ```
-If `frontend-public/src/frontend/public` is updated, this command can be used to create a new branch in `test-repo` and sync the changes made in `frontend-public/src/frontend/public` to that branch. Later, a pull request can be made to merge these changes with the master branch.
+If `frontend-public/src/frontend/public` is updated, this command can be used to create a new branch names `photos` in `test-repo` and sync the changes made in `frontend-public/src/frontend/public` to that branch. Later, a pull request can be created to merge these changes with the master branch.
--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const argv = require("minimist")(process.argv.slice(2));
-const path = require('path');
+const path = require("path");
 const initializeFolder = require("../lib/init.js");
 const updateFolderFromMain = require("../lib/pull.js");
 const updateMainFromFolder = require("../lib/push.js");
@@ -9,19 +9,25 @@ async function parseArgs(currentDir, command, args) {
   switch (command) {
     case "init":
       // git-fork init [repo path] [first/folder/path/from/root] [second/folder/path/from/root] [local folder name]
-      const repo = args[0];
-      const folders = args.slice(1, -1);
-      const forkedRepo = args[args.length - 1];
-      const repoName = path.basename(repo);
-      const forkedName = path.basename(forkedRepo);
-      const folderPaths = folders.map(fd => `${repoName}/${fd}`);
-      console.log(`Initializing ${forkedName} from ${folderPaths.join(', ')} ...`);
-      await initializeFolder(
-        path.resolve(currentDir, repo),
-        folders,
-        path.resolve(currentDir, forkedRepo)
-      );
-      console.log(`Successfully forked into ${forkedRepo}`);
+      if (args.length > 2) {
+        const repo = args[0];
+        const folders = args.slice(1, -1);
+        const forkedRepo = args[args.length - 1];
+        const repoName = path.basename(repo);
+        const forkedName = path.basename(forkedRepo);
+        const folderPaths = folders.map(fd => `${repoName}/${fd}`);
+        console.log(
+          `Initializing ${forkedName} from ${folderPaths.join(", ")} ...`
+        );
+        await initializeFolder(
+          path.resolve(currentDir, repo),
+          folders,
+          path.resolve(currentDir, forkedRepo)
+        );
+        console.log(`Successfully forked into ${forkedRepo}`);
+      } else {
+        console.log("Error: Invalid arguments");
+      }
       break;
     case "pull":
       // git-fork pull
@@ -29,14 +35,18 @@ async function parseArgs(currentDir, command, args) {
       break;
     case "push":
       // git-fork push [branch name] [commit message]
-      const [ commitMsg, branchName ] = args;
-      updateMainFromFolder(currentDir, commitMsg, branchName);
+      const [commitMsg, branchName] = args;
+      if (commitMsg && branchName) {
+        updateMainFromFolder(currentDir, commitMsg, branchName);
+      } else {
+        console.log("Error: Invalid arguments");
+      }
       break;
     default:
     // showHelp();
   }
 }
 
-const { _: [ command, ...args ] } = argv;
+const { _: [command, ...args] } = argv;
 const dir = process.cwd();
 parseArgs(dir, command, args);
