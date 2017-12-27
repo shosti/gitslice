6654c7ae2dc87ede779926c833cd12aaa320c847
Arslan Arshad <arslanarshad.31@gmail.com>
2017-12-27 21:23:59 +0500

    cannot pull with uncommitted changes

--- tests/pull.test.js
+++ tests/pull.test.js
@@ -3,7 +3,7 @@ const { CONFIG_FILENAME } = require("../lib/constants");
 const Git = require("nodegit");
 const path = require("path");
 const fs = require("fs-extra");
-const { getCurBranch } = require("../lib/utils");
+const { getCurBranch, getAllFiles } = require("../lib/utils");
 
 const mainRepoRelativePath = "./repos/pull/main";
 const folderRepoRelativePath = "./repos/pull/folder";
@@ -169,31 +169,31 @@ describe("Folder repo is synced properly with main repo", () => {
   test("does not pull if there are uncommitted changes", async () => {
     expect.assertions(1);
     const testFile1Path = path.resolve(
-      mainRepoPath,
+      folderRepoPath,
       folderPaths[0],
       "testFile1.txt"
     );
     const testFile2Path = path.resolve(
-      mainRepoPath,
+      folderRepoPath,
       folderPaths[1],
       "testFile2.txt"
     );
     const testFile3Path = path.resolve(
-      mainRepoPath,
+      folderRepoPath,
       folderPaths[1],
       "testFile3.txt"
     );
-    const testFile1Text = "Hello World!";
-    const testFile2Text = "How are you?";
-    const testFile3Text = "I am good";
+    const testFile1Text = "Some unimportant text";
+    const testFile2Text = "Testing Testing Testing";
+    const testFile3Text = "I want to travel";
     await fs.outputFile(testFile1Path, testFile1Text);
     await fs.outputFile(testFile2Path, testFile2Text);
     await fs.outputFile(testFile3Path, testFile3Text);
+
     try {
       await parseArgsAndExecute(folderRepoPath, ["pull"]);
     } catch (e) {
       expect(e).toBe("Error: cannot pull with uncommitted changes");
     }
   });
-
 });
